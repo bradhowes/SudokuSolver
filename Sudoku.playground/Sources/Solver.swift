70176fda319b45138a5cde67f2126dfea91d8873
Brad Howes <bradhowes@mac.com>
2022-08-02 17:11:14 +0200

    Fixed detection of multiple solutions.

--- Sudoku.playground/Sources/Solver.swift
+++ Sudoku.playground/Sources/Solver.swift
@@ -6,27 +6,11 @@ public enum Sudoku {
   static let unknown = 0
 
   public static func solve(board: [[Int]]) -> (board: [[Int]], solved: Bool, unique: Bool) {
-    let clueCount = board.reduce(0) { $0 + $1.reduce(0) { $0 + ($1 == Sudoku.unknown ? 0 : 1) } }
-    var first = Solver(board: board)
-    let solved = first.solve(rotation: 0)
-    if !solved {
-      return (board: first.board, solved: false, unique: false)
-    }
-
-    // See if there are other solutions. NOTE this approach can give false results since it does not try all
-    // combinations of "picks"
-    var maybeUnique = clueCount > 16
-    if maybeUnique {
-      for rotation in 0..<size {
-        var alternative = Solver(board: board)
-        let solved = alternative.solve(rotation: rotation)
-        if solved && alternative.board != first.board {
-          maybeUnique = false
-          break
-        }
-      }
-    }
-    return (board: first.board, solved: true, unique: maybeUnique)
+    var solver = Solver(board: board)
+    let solved = solver.solve()
+    var counter = Solver(board: board)
+    let count = counter.countAll()
+    return (board: solver.board, solved: solved, unique: count == 1)
   }
 }
 
@@ -45,12 +29,14 @@ fileprivate struct Solver {
   private(set) var board: [[Int]]
   // The candidate values available for each row
   private var rows: [Set<Int>] = .filled()
-  // The picked values for each row. The intersection of rows and picked should always be empty.
-  private var picked: [Set<Int>] = .empty()
   // The candidate values available for each column
   private var columns: [Set<Int>] = .filled()
   // The candidate values available for each box
   private var boxes: [Set<Int>] = .filled()
+  // The picked values for each row. The solver iterates over the available values of a row, but the row is evaluated
+  // recursively for all open positions so we need to a way to modify the row picks without disturbing the iteration
+  // being done elsewhere.
+  private var picked: [Set<Int>] = .empty()
 
   init(board: [[Int]]) {
     precondition(board.count == Sudoku.size)
@@ -58,16 +44,15 @@ fileprivate struct Solver {
     self.board = board
 
     // Initialize the various sets used to track picks for missing numbers.
-    for rowIndex in 0..<Sudoku.size {
-      for colIndex in 0..<Sudoku.size {
-        let value = board[rowIndex][colIndex]
+    for row in 0..<Sudoku.size {
+      for col in 0..<Sudoku.size {
+        let value = board[row][col]
         if value != Sudoku.unknown {
           precondition(value >= 1 && value <= Sudoku.size)
-
           // Remove value from candidates in various collections
-          rows[rowIndex].remove(value)
-          columns[colIndex].remove(value)
-          boxes[boxIndex(rowIndex: rowIndex, colIndex: colIndex)].remove(value)
+          rows[row].remove(value)
+          columns[col].remove(value)
+          boxes[boxIndex(row: row, col: col)].remove(value)
         }
       }
     }
@@ -78,79 +63,91 @@ fileprivate struct Solver {
    This includes setups that have more than one solution. Currently there is an imprecise check if a board leads to
    multiple solutions.
 
-   - parameter rotation rotate initial candidates by this amount to search for alternative solutions
    - returns: true if unique solution found
    */
-  mutating func solve(rotation: Int) -> Bool {
-    return solve(rowIndex: 0, colIndex: 0, rotation: rotation)
+  mutating func solve() -> Bool {
+    return solve(row: 0, col: 0)
   }
 
-  private func candidates(rowIndex: Int, colIndex: Int, rotation: Int) -> [Int] {
-    let unpicked = Array(rows[rowIndex].filter { !picked[rowIndex].contains($0) })
-    if rotation == 0 {
-      return unpicked
-    } else if rotation < unpicked.count {
-      return Array(unpicked.dropFirst(rotation) + unpicked.prefix(rotation))
-    } else {
-      return []
-    }
+  mutating func countAll() -> Int {
+    return countAll(row: 0, col: 0)
   }
 
-  private mutating func solve(rowIndex: Int, colIndex: Int, rotation: Int = 0) -> Bool {
-
+  private mutating func solve(row: Int, col: Int) -> Bool {
     // Terminating condition when all rows are done
-    guard rowIndex < Sudoku.size else { return true }
-
+    guard row < Sudoku.size else { return true }
     // Move to next row if end of columns
-    guard colIndex < Sudoku.size else {
-      return solve(rowIndex: rowIndex + 1, colIndex: 0, rotation: rotation)
-    }
-
+    guard col < Sudoku.size else { return solve(row: row + 1, col: 0) }
     // Move to next column if value is known
-    guard board[rowIndex][colIndex] == Sudoku.unknown else {
-      return solve(rowIndex: rowIndex, colIndex: colIndex + 1, rotation: 0)
-    }
+    guard board[row][col] == Sudoku.unknown else { return solve(row: row, col: col + 1) }
 
-    // Visit all of the unknown values in the row and try it out as a candidate
-    var result = false
-    for value in candidates(rowIndex: rowIndex, colIndex: colIndex, rotation: rotation) {
-      let boxIndex = self.boxIndex(rowIndex: rowIndex, colIndex: colIndex)
+    // Visit all of the unknown values in the row and find the first that leads to a solution.
+    for value in rows[row].filter({ !picked[row].contains($0) }) {
+      let box = self.boxIndex(row: row, col: col)
 
       // Only consider candidate if it is also a candidate in the column and box we are working in
-      if columns[colIndex].contains(value) && boxes[boxIndex].contains(value) {
-        pick(value, rowIndex: rowIndex, colIndex: colIndex, boxIndex: boxIndex)
+      if columns[col].contains(value) && boxes[box].contains(value) {
+        pick(value, row: row, col: col, box: box)
 
         // Recurse to the next column. If true, then the candidate worked.
-        if solve(rowIndex: rowIndex, colIndex: colIndex + 1, rotation: 0) {
-          result = true
-          break
-        } else {
-
-          // Candidate did not work -- backtrack by removing it.
-          unpick(value, rowIndex: rowIndex, colIndex: colIndex, boxIndex: boxIndex)
+        if solve(row: row, col: col + 1) {
+          return true
         }
+
+        // Candidate did not work -- backtrack by removing it.
+        unpick(value, row: row, col: col, box: box)
       }
     }
+
+    return false
+  }
+
+  private mutating func countAll(row: Int, col: Int) -> Int {
+    // Terminating condition when all rows are done
+    guard row < Sudoku.size else { return 1 }
+    // Move to next row if end of columns
+    guard col < Sudoku.size else { return countAll(row: row + 1, col: 0) }
+    // Move to next column if value is known
+    guard board[row][col] == Sudoku.unknown else { return countAll(row: row, col: col + 1) }
+
+    // Visit all of the unknown values in the row and try each as a candidate
+    var result = 0
+    for value in rows[row].filter({ !picked[row].contains($0) }) {
+      let box = self.boxIndex(row: row, col: col)
+
+      // Only consider candidate if it is also a candidate in the column and box we are working in
+      if columns[col].contains(value) && boxes[box].contains(value) {
+        pick(value, row: row, col: col, box: box)
+
+        // Recurse to the next column and record how many solutions were found
+        let found = countAll(row: row, col: col + 1)
+        result += found
+
+        // Undo all picks before moving to next candidate
+        unpick(value, row: row, col: col, box: box)
+      }
+    }
+
     return result
   }
 
-  private mutating func pick(_ value: Int, rowIndex: Int, colIndex: Int, boxIndex: Int) {
+  private mutating func pick(_ value: Int, row: Int, col: Int, box: Int) {
     precondition(value >= 1 && value <= Sudoku.size)
-    board[rowIndex][colIndex] = value
-    picked[rowIndex].insert(value)
-    columns[colIndex].remove(value)
-    boxes[boxIndex].remove(value)
+    board[row][col] = value
+    picked[row].insert(value)
+    columns[col].remove(value)
+    boxes[box].remove(value)
   }
 
-  private mutating func unpick(_ value: Int, rowIndex: Int, colIndex: Int, boxIndex: Int) {
-    board[rowIndex][colIndex] = Sudoku.unknown
-    picked[rowIndex].remove(value)
-    columns[colIndex].insert(value)
-    boxes[boxIndex].insert(value)
+  private mutating func unpick(_ value: Int, row: Int, col: Int, box: Int) {
+    board[row][col] = Sudoku.unknown
+    picked[row].remove(value)
+    columns[col].insert(value)
+    boxes[box].insert(value)
   }
 
-  private func boxIndex(rowIndex: Int, colIndex: Int) -> Int {
-    rowIndex / Sudoku.boxSize * Sudoku.boxSize + colIndex / Sudoku.boxSize
+  private func boxIndex(row: Int, col: Int) -> Int {
+    row / Sudoku.boxSize * Sudoku.boxSize + col / Sudoku.boxSize
   }
 }
 
