571d65542a499094351f1b0184a3c6f91e55a592
Brad Howes <bradhowes@mac.com>
2023-05-05 09:23:12 -0400

    Light cleanup

--- Sudoku.playground/Sources/SolutionViewBuilder.swift
+++ Sudoku.playground/Sources/SolutionViewBuilder.swift
@@ -8,64 +8,38 @@ public enum SolutionViewBuilder {
 
 private func makeBlocks(config: [[Int]], solution: [[Int]]?) -> UIStackView {
   let rows = UIStackView()
-
-  rows.backgroundColor = .black
-  rows.axis = .vertical
-  rows.distribution = .equalSpacing
-  rows.spacing = 4
+  configureStackView(rows, axis: .vertical, distribution: .equalSpacing, spacing: 4)
   rows.translatesAutoresizingMaskIntoConstraints = false
-
-//  rows.addArrangedSubview(makeTitle(name: name))
-
   for rowIndex in stride(from: 0, to: 9, by: 3) {
     let row = UIStackView()
-    row.backgroundColor = .black
-    row.axis = .horizontal
-    row.distribution = .fillEqually
-    row.spacing = 4
-
+    configureStackView(row, axis: .horizontal, distribution: .fillEqually, spacing: 4)
     for colIndex in stride(from: 0, to: 9, by: 3) {
-      let block = makeCellBlock(rowIndex: rowIndex, colIndex: colIndex, config: config, solution: solution)
-      row.addArrangedSubview(block)
+      row.addArrangedSubview(makeCellBlock(rowIndex: rowIndex, colIndex: colIndex, config: config, solution: solution))
     }
-
     rows.addArrangedSubview(row)
   }
-
-//  rows.addArrangedSubview(makeState(solved: solved, unique: unique))
   return rows
 }
 
 private func makeCellBlock(rowIndex: Int, colIndex: Int, config: [[Int]], solution: [[Int]]?) -> UIStackView {
   let block = UIStackView()
-  block.backgroundColor = .black
-  block.axis = .vertical
-  block.distribution = .equalSpacing
-  block.spacing = 2
-
+  configureStackView(block, axis: .vertical, distribution: .equalSpacing, spacing: 2)
   for rowOffset in 0..<3 {
     block.addArrangedSubview(makeCellRow(rowIndex: rowIndex + rowOffset, colIndex: colIndex, config: config,
                                          solution: solution))
   }
-
   return block
 }
 
 private func makeCellRow(rowIndex: Int, colIndex: Int, config: [[Int]], solution: [[Int]]?) -> UIStackView {
   let row = UIStackView()
-  row.backgroundColor = .black
-  row.axis = .horizontal
-  row.distribution = .fillEqually
-  row.spacing = 2
-
+  configureStackView(row, axis: .horizontal, distribution: .fillEqually, spacing: 2)
   let configRow = config[rowIndex]
   let solvedRow = solution?[rowIndex] ?? configRow
-
   for colOffset in 0..<3 {
     let index = colIndex + colOffset
     row.addArrangedSubview(makeCell(value: solvedRow[index], config: configRow[index]))
   }
-
   return row
 }
 
@@ -78,39 +52,10 @@ private func makeCell(value: Int, config: Int) -> UILabel {
   return label
 }
 
-//private func makeTitle(name: String) -> UILabel {
-//  let label = UILabel()
-//  label.font = .systemFont(ofSize: 18)
-//  label.backgroundColor = .white
-//  label.textAlignment = .center
-//  label.text = name
-//  label.textColor = .black
-//  return label
-//}
-//
-//private func makeState(solved: Bool, unique: Bool) -> UIStackView {
-//  let row = UIStackView()
-//  row.backgroundColor = .white
-//  row.axis = .horizontal
-//  row.distribution = .fillEqually
-//  row.spacing = 8
-//
-//  let label1 = UILabel()
-//  label1.font = .systemFont(ofSize: 18)
-//  label1.backgroundColor = .white
-//  label1.textAlignment = .center
-//  label1.text = "Solved: \(solved)"
-//  label1.textColor = solved ? .black : .systemRed
-//  row.addArrangedSubview(label1)
-//
-//  let label2 = UILabel()
-//  label2.font = .systemFont(ofSize: 18)
-//  label2.backgroundColor = .white
-//  label2.textAlignment = .center
-//  label2.text = " Unique: \(unique)"
-//  label2.textColor = unique ? .black : .systemRed
-//  row.addArrangedSubview(label2)
-//
-//  return row
-//}
-//
+private func configureStackView(_ stackView: UIStackView, axis: NSLayoutConstraint.Axis,
+                                distribution: UIStackView.Distribution, spacing: CGFloat) {
+  stackView.backgroundColor = .black
+  stackView.axis = axis
+  stackView.distribution = distribution
+  stackView.spacing = spacing
+}
--- Sudoku.playground/Sources/SudokuSolver.swift
+++ Sudoku.playground/Sources/SudokuSolver.swift
@@ -12,7 +12,7 @@ public enum Sudoku {
 }
 
 fileprivate extension Set where Element == Int {
-  static func filled() -> Set { .init([1, 2, 3, 4, 5, 6, 7, 8, 9]) }
+  static func filled() -> Set { .init(1...9) }
 }
 
 fileprivate extension Array where Element == Set<Int> {
@@ -23,7 +23,7 @@ fileprivate extension Array where Element == Set<Int> {
 fileprivate struct Solver {
   // The state of the solution being filled in by the solver
   private(set) var board: [[Int]]
-  // Set to the first solution found by the solver
+  // Collection of solutions found by the solver
   private(set) var solutions = [[[Int]]]()
   // The candidate values available for each row
   private var rows: [Set<Int>] = .filled()
@@ -46,7 +46,7 @@ fileprivate struct Solver {
           // Remove value from candidates in various collections
           rows[row].remove(value)
           columns[col].remove(value)
-          boxes[boxIndex(row: row, col: col)].remove(value)
+          boxes[toBox(row: row, col: col)].remove(value)
         }
       }
     }
@@ -65,27 +65,19 @@ fileprivate struct Solver {
   private mutating func solve(row: Int, col: Int) {
     if row == Sudoku.size {
       solutions.append(board)
-    }
-    else if col == Sudoku.size {
+    } else if col == Sudoku.size {
       solve(row: row + 1, col: 0)
-    }
-    else if board[row][col] != Sudoku.unknown {
+    } else if board[row][col] != Sudoku.unknown {
       solve(row: row, col: col + 1)
-    }
-    else {
-      for value in rows[row] {
-        let box = self.boxIndex(row: row, col: col)
-
-        // Only consider candidate if it is also a candidate in the column and box we are working in
-        if columns[col].contains(value) && boxes[box].contains(value) {
-          pick(value, row: row, col: col, box: box)
-
-          // Recurse to the next column and record how many solutions were found
-          solve(row: row, col: col + 1)
-
-          // Undo all picks before moving to next candidate
-          unpick(value, row: row, col: col, box: box)
-        }
+    } else {
+      let box = toBox(row: row, col: col)
+      // Only consider candidate if it is also a candidate in the column and box we are working in
+      for value in rows[row] where columns[col].contains(value) && boxes[box].contains(value) {
+        pick(value, row: row, col: col, box: box)
+        // Recurse to the next column and record how many solutions were found
+        solve(row: row, col: col + 1)
+        // Undo all picks before moving to next candidate
+        unpick(value, row: row, col: col, box: box)
       }
     }
   }
@@ -103,8 +95,8 @@ fileprivate struct Solver {
     columns[col].insert(value)
     boxes[box].insert(value)
   }
+}
 
-  private func boxIndex(row: Int, col: Int) -> Int {
-    row / Sudoku.boxSize * Sudoku.boxSize + col / Sudoku.boxSize
-  }
+private func toBox(row: Int, col: Int) -> Int {
+  (col < 3 ? 0 : (col < 6 ? 1 : 2)) + (row < 3 ? 0 : (row < 6 ? 3 : 6))
 }
