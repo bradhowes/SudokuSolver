f780a1ed10f7d5c2446bed4e7e6bd93ffcced106
Brad Howes <bradhowes@mac.com>
2026-03-27 20:02:14 +0100

    Reworked to use SwiftUI

--- Sudoku.playground/Contents.swift
+++ Sudoku.playground/Contents.swift
@@ -1,163 +1,203 @@
-import UIKit
 import PlaygroundSupport
 import CoreGraphics
+import SwiftUI
+
+let blah =
+"53  7    " +
+"6  195   " +
+" 98    6 " +
+"8   6   3" +
+"4  8 3  1" +
+"7   2   6" +
+" 6    28 " +
+"   419  5" +
+"    8  79"
 
 let easy =
-  "  79  281" +
-  "628  3 9 " +
-  "4 9     6" +
-  "7  6 9832" +
-  "1 3      " +
-  "8       9" +
-  "  6378 2 " +
-  " 8 49167 " +
-  " 71 2694 "
+"  79  281" +
+"628  3 9 " +
+"4 9     6" +
+"7  6 9832" +
+"1 3      " +
+"8       9" +
+"  6378 2 " +
+" 8 49167 " +
+" 71 2694 "
 
 let diabolical =
-  "    9 16 " +
-  "63  2    " +
-  " 2       " +
-  "  26     " +
-  "4  1 5  3" +
-  "17       " +
-  "9    4  6" +
-  "     78 4" +
-  "  3    2 "
+"    9 16 " +
+"63  2    " +
+" 2       " +
+"  26     " +
+"4  1 5  3" +
+"17       " +
+"9    4  6" +
+"     78 4" +
+"  3    2 "
 
 let multiple =
-  "  6 7 4 3" +
-  "   4  2  " +
-  " 7  23 1 " +
-  "5     1  " +
-  " 4 2 8 6 " +
-  "  3     5" +
-  " 3 7   5 " +
-  "  7  5   " +
-  "4 5 1 7  "
+"  6 7 4 3" +
+"   4  2  " +
+" 7  23 1 " +
+"5     1  " +
+" 4 2 8 6 " +
+"  3     5" +
+" 3 7   5 " +
+"  7  5   " +
+"4 5 1 7  "
 
 let multiple2 =
-  "926571483" +
-  "351486279" +
-  "874923516" +
-  "582367194" +
-  "149258367" +
-  "7631  825" +
-  "2387  651" +
-  "617835942" +
-  "495712738"
+"926571483" +
+"351486279" +
+"874923516" +
+"582367194" +
+"149258367" +
+"7631  825" +
+"2387  651" +
+"617835942" +
+"495712738"
 
 let unsolvable =
-  "3 79  281" +
-  "628  3 9 " +
-  "4 9     6" +
-  "7  6 9832" +
-  "1 3      " +
-  "8       9" +
-  "  6378 2 " +
-  " 8 49167 " +
-  " 71 2694 "
+"3 79  281" +
+"628  3 9 " +
+"4 9     6" +
+"7  6 9832" +
+"1 3      " +
+"8       9" +
+"  6378 2 " +
+" 8 49167 " +
+" 71 2694 "
 
-class MyViewController : UIViewController {
+struct SudokuView: View {
+  @StateObject private var viewModel = SudokuViewModel()
 
-  var mainView: UIStackView!
-  var prevPuzzleButton: UIButton!
-  var puzzleTitle: UILabel!
-  var nextPuzzleButton: UIButton!
-  var puzzleView: UIStackView?
-  var puzzleViewBackgroundView: UIView?
-  var solutionState: UILabel!
-  var prevSolutionButton: UIButton!
-  var nextSolutionButton: UIButton!
-
-  let puzzles = [easy, diabolical, multiple, multiple2, unsolvable]
-  let names = ["Easy", "Diabolical", "Multiple", "Multiple 2", "Unsolvable"]
-  var puzzleIndex = 0
-  var solutions: [[[Int]]] = []
-  var solutionIndex = 0
-
-  let prevButtonText = "◀"
-  let nextButtonText = "▶"
-
-  private func pinBackground(_ view: UIView, to stackView: UIStackView) {
-    view.translatesAutoresizingMaskIntoConstraints = false
-    stackView.insertSubview(view, at: 0)
-    view.pin(to: stackView, border: 4)
+  private var solutionStatusText: String {
+    switch viewModel.solutions.count {
+    case 0:
+      return "Unsolvable"
+    case 1:
+      return "Unique"
+    default:
+      return "\(viewModel.solutionIndex + 1) of \(viewModel.solutions.count)"
+    }
   }
 
-  override func loadView() {
-    let view = UIView()
-    view.backgroundColor = .systemBackground
-    self.view = view
-    makeMainView()
-    showPuzzle()
+  var body: some View {
+    VStack(spacing: 0) {
+      HStack(spacing: 4) {
+        Button {
+          viewModel.previousPuzzle()
+        } label: {
+          Text("◀")
+        }
+        .disabled(viewModel.puzzles.isEmpty)
+
+        Text(viewModel.names[viewModel.puzzleIndex])
+          .font(.system(size: 18))
+          .frame(maxWidth: .infinity)
+          .padding(.vertical, 8)
+          .background(Color.white)
+          .foregroundColor(.black)
+
+        Button {
+          viewModel.nextPuzzle()
+        } label: {
+          Text("▶")
+        }
+        .disabled(viewModel.puzzles.isEmpty)
+      }
+
+      SudokuPuzzleView(
+        board: viewModel.puzzles[viewModel.puzzleIndex],
+        solution: viewModel.solutions.isEmpty ? viewModel.puzzles[viewModel.puzzleIndex] : viewModel.solutions[viewModel.solutionIndex]
+      )
+
+      HStack(spacing: 4) {
+        Button {
+          viewModel.previousSolution()
+        } label: {
+          Text("◀")
+        }
+        .disabled(viewModel.solutions.isEmpty)
+
+        Text(viewModel.solutionStatusText)
+          .font(.system(size: 18))
+          .frame(maxWidth: .infinity)
+          .padding(.vertical, 8)
+          .foregroundColor(solutionStatusColor)
+
+        Button {
+          viewModel.nextSolution()
+        } label: {
+          Text("▶")
+        }
+        .disabled(viewModel.solutions.isEmpty)
+      }
+    }
+    .animation(.smooth, value: viewModel.puzzleIndex)
+    .animation(.smooth, value: viewModel.solutionIndex)
+    .padding()
   }
 
-  private func makeMainView() {
-    mainView = UIStackView()
-    mainView.axis = .vertical
-    mainView.distribution = .equalSpacing
-    mainView.spacing = 8
-    mainView.translatesAutoresizingMaskIntoConstraints = false
-    view.addCenteredSubview(mainView)
-
-    makeTitle()
-    makeStatus()
+  var solutionStatusColor: Color {
+    switch viewModel.solutions.count {
+    case 0:
+      return .red
+    case 1:
+      return .green
+    default:
+      return .yellow
+    }
   }
+}
 
-  private func makeTitle() {
-    let topRow = UIStackView()
-    topRow.axis = .horizontal
-    topRow.distribution = .fillEqually
-    topRow.spacing = 4
+struct SudokuPuzzleView: View {
+  let board: [[Int]]
+  let solution: [[Int]]
+  let cellSize: CGFloat = 40
 
-    prevPuzzleButton = UIButton(type: .system)
-    prevPuzzleButton.setTitle(prevButtonText, for: .normal)
-    prevPuzzleButton.addTarget(self, action: #selector(previousPuzzle), for: .touchDown)
-    topRow.addArrangedSubview(prevPuzzleButton)
-
-    puzzleTitle = UILabel()
-    puzzleTitle.font = .systemFont(ofSize: 18)
-    puzzleTitle.backgroundColor = .white
-    puzzleTitle.textAlignment = .center
-    puzzleTitle.text = ""
-    puzzleTitle.textColor = .black
-    topRow.addArrangedSubview(puzzleTitle)
-
-    nextPuzzleButton = UIButton(type: .system)
-    nextPuzzleButton.setTitle(nextButtonText, for: .normal)
-    nextPuzzleButton.addTarget(self, action: #selector(nextPuzzle), for: .touchDown)
-    topRow.addArrangedSubview(nextPuzzleButton)
-
-    mainView.addArrangedSubview(topRow)
+  var body: some View {
+    VStack(spacing: 0) {
+      ForEach(0..<9, id: \.self) { row in
+        HStack(spacing: 0) {
+          ForEach(0..<9, id: \.self) { col in
+            CellView(value: solution[row][col], color: board[row][col] == 0 ? .green : .black)
+              .frame(width: cellSize, height: cellSize)
+              .background(.white)
+              .border(Color.black, width: 0.5)
+              .padding(.leading, (col % 3) == 0 ? 2 : 0)
+          }
+        }
+        // .padding(.horizontal, 2)
+        .padding(.top, (row % 3) == 0 ? 2 : 0)
+      }
+    }
+    .padding(.trailing, 2)
+    .padding(.bottom, 2)
+    .background(.black)
   }
+}
 
-  private func makeStatus() {
-    let statusRow = UIStackView()
-    statusRow.axis = .horizontal
-    statusRow.distribution = .fillEqually
-    statusRow.spacing = 4
+struct CellView: View {
+  let value: Int
+  let color: Color
 
-    prevSolutionButton = UIButton(type: .system)
-    prevSolutionButton.setTitle(prevButtonText, for: .normal)
-    prevSolutionButton.addTarget(self, action: #selector(previousSolution), for: .touchDown)
-    statusRow.addArrangedSubview(prevSolutionButton)
-
-    solutionState = UILabel()
-    solutionState.font = .systemFont(ofSize: 18)
-    solutionState.text = "Unknown"
-    solutionState.textAlignment = .center
-    solutionState.textColor = .systemRed
-    statusRow.addArrangedSubview(solutionState)
-
-    nextSolutionButton = UIButton(type: .system)
-    nextSolutionButton.setTitle(nextButtonText, for: .normal)
-    nextSolutionButton.addTarget(self, action: #selector(nextSolution), for: .touchDown)
-    statusRow.addArrangedSubview(nextSolutionButton)
-
-    mainView.addArrangedSubview(statusRow)
+  var body: some View {
+    Text(value == 0 ? "?" : String(value))
+      .font(.system(size: 18, weight: .semibold))
+      .foregroundColor(color)
   }
+}
 
-  func decode(_ encoded: String) -> [[Int]] {
+class SudokuViewModel: ObservableObject {
+  @Published var puzzleIndex = 0
+  @Published var solutionIndex = 0
+  @Published var solutions: [[[Int]]] = []
+
+  let puzzles = [blah, easy, diabolical, multiple, multiple2, unsolvable].map { decode($0) }
+  let names = ["Blah", "Easy", "Diabolical", "Multiple", "Multiple 2", "Unsolvable"]
+
+  static func decode(_ encoded: String) -> [[Int]] {
     precondition(encoded.count == 9 * 9)
     return stride(from: 0, to: encoded.count, by: 9).map {
       let start = encoded.index(encoded.startIndex, offsetBy: $0)
@@ -166,112 +206,75 @@ class MyViewController : UIViewController {
     }
   }
 
-  func showPuzzle() {
-    puzzleTitle.text = names[puzzleIndex]
-    let puzzle = decode(puzzles[puzzleIndex])
-    solutions = Sudoku.solve(board: puzzle, findOne: false)
-    solutionIndex = 0
-    showSolution()
-  }
+  var currentPuzzleName: String { names[puzzleIndex] }
 
-  func showSolution() {
-    puzzleView?.removeFromSuperview()
-    puzzleViewBackgroundView?.removeFromSuperview()
-    puzzleViewBackgroundView = UIView()
-    puzzleViewBackgroundView?.backgroundColor = .black
-
-    let puzzle = decode(puzzles[puzzleIndex])
-    let puzzleView = SolutionViewBuilder.buildFrom(config: puzzle, solution: solutions.isEmpty ? nil : solutions[solutionIndex])
-    self.puzzleView = puzzleView
-
-    pinBackground(puzzleViewBackgroundView!, to: puzzleView)
-    mainView.insertArrangedSubview(puzzleView, at: 1)
-
+  var solutionStatusText: String {
     switch solutions.count {
     case 0:
-      solutionState.text = "Unsolvable"
-      solutionState.textColor = .systemRed
-      break
-
+      return "Unsolvable"
     case 1:
-      solutionState.text = "Unique"
-      solutionState.textColor = .systemGreen
-
+      return "Unique"
     default:
-      solutionState.text = "\(solutionIndex + 1) of \(solutions.count)"
-      solutionState.textColor = .systemYellow
+      return "\(solutionIndex + 1) of \(solutions.count)"
     }
-
-    prevSolutionButton.isEnabled = solutions.count > 1
-    nextSolutionButton.isEnabled = solutions.count > 1
-
-    view.setNeedsLayout()
   }
 
-  func addButtons() -> UIStackView {
-    let row = UIStackView()
-    row.backgroundColor = .white
-    row.axis = .horizontal
-    row.distribution = .fillEqually
-    row.spacing = 8
-    row.translatesAutoresizingMaskIntoConstraints = false
-    return row
+  var solutionStatusColor: Color {
+    switch solutions.count {
+    case 0:
+      return .red
+    case 1:
+      return .green
+    default:
+      return .yellow
+    }
   }
 
-  @objc func previousPuzzle(sender: UIButton) {
-    puzzleIndex = (puzzleIndex == 0 ? puzzles.count : puzzleIndex) - 1
-    showPuzzle()
+  init() {
+    solvePuzzle()
   }
 
-  @objc func nextPuzzle(sender: UIButton) {
-    puzzleIndex = (puzzleIndex == puzzles.count - 1 ? -1 : puzzleIndex) + 1
-    showPuzzle()
+  func previousPuzzle() {
+    if puzzleIndex == 0 {
+      puzzleIndex = puzzles.count - 1
+    } else {
+      puzzleIndex -= 1
+    }
+    solvePuzzle()
   }
 
-  @objc func previousSolution(sender: UIButton) {
-    solutionIndex = (solutionIndex == 0 ? solutions.count : solutionIndex) - 1
-    showSolution()
+  func nextPuzzle() {
+    if puzzleIndex == puzzles.count - 1 {
+      puzzleIndex = 0
+    } else {
+      puzzleIndex += 1
+    }
+    solvePuzzle()
   }
 
-  @objc func nextSolution(sender: UIButton) {
-    solutionIndex = (solutionIndex == solutions.count - 1 ? -1 : solutionIndex) + 1
-    showSolution()
+  func previousSolution() {
+    if solutionIndex == 0 {
+      solutionIndex = solutions.count - 1
+    } else {
+      solutionIndex -= 1
+    }
+  }
+
+  func nextSolution() {
+    if solutionIndex == solutions.count - 1 {
+      solutionIndex = 0
+    } else {
+      solutionIndex += 1
+    }
+  }
+
+  private func solvePuzzle() {
+    solutions = Sudoku.solve(board: puzzles[puzzleIndex], findOne: false)
+    solutionIndex = 0
   }
 }
 
-extension UIView {
-
-  func addCenteredSubview(_ child: UIView) {
-    addSubview(child)
-    let dict = ["view": self, "child": child]
-    addConstraints(
-      NSLayoutConstraint.constraints(
-        withVisualFormat: "V:[view]-(<=1)-[child]-(<=1)-[view]", options: .alignAllCenterX, metrics: nil, views: dict) +
-      NSLayoutConstraint.constraints(
-        withVisualFormat: "H:[view]-(<=1)-[child]-(<=1)-[view]", options: .alignAllCenterY, metrics: nil, views: dict)
-    )
-  }
-
-  func pin(to view: UIView, border: CGFloat) {
-    let constraints = [
-      leadingAnchor.constraint(equalTo: view.leadingAnchor),
-      trailingAnchor.constraint(equalTo: view.trailingAnchor),
-      topAnchor.constraint(equalTo: view.topAnchor),
-      bottomAnchor.constraint(equalTo: view.bottomAnchor)
-    ]
-
-    for each in constraints {
-      if each.secondAnchor == view.leadingAnchor || each.secondAnchor == view.topAnchor {
-        each.constant = -border
-      }
-      else {
-        each.constant = border
-      }
-    }
-
-    NSLayoutConstraint.activate(constraints)
-  }
-}
-
-// Present the view controller in the Live View window
-PlaygroundPage.current.liveView = MyViewController()
+PlaygroundPage.current.setLiveView(
+  SudokuView()
+    .previewLayout(.fixed(width: 500, height: 800))
+)
